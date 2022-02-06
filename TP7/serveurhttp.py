3c3027c43637d476b99acc6173f925ca2c1c2e94
Lai Khang Duy <laiduy98@Lais-MacBook-Pro.local>
2022-02-06 17:00:30 +0100

    tp7 html

--- TP7/serveurhttp.py
+++ TP7/serveurhttp.py
@@ -1,9 +1,10 @@
 import socket
+import os
 
 
 # Define socket host and port
-HOST = '0.0.0.0'
-PORT = 6667
+HOST = '127.0.0.1'
+PORT = 8000
 
 # Create socket
 sserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,17 +14,27 @@ sserver.listen(1)
 print('Listening on port %s ...' % PORT)
 
 while True:
-    # Wait for client connections
     client_connection, client_address = sserver.accept()
 
-    # Get the client request
     request = client_connection.recv(1024).decode()
-    print(request)
+    # print(request)
+    get_path = request.split('/r/n')[0].split()[1]
+    print(get_path)
 
-    # Send HTTP response
-    response = 'HTTP/1.0 200 OK\n\nPROG-AV'
-    client_connection.sendall(response.encode())
-    client_connection.close()
+    if os.path.isfile(get_path[1:]):
+        with open(get_path[1:], 'r') as f:
+            data = f.read()
+        response = f'HTTP/1.0 200 OK\n\n{data}'
+        client_connection.sendall(response.encode())
+        client_connection.close()
+    elif get_path == '/':
+        response = 'HTTP/1.0 200 OK\n\nPROG-AV'
+        client_connection.sendall(response.encode())
+        client_connection.close()
+    else:
+        response = 'HTTP/1.0 200 OK\n\nWrong Path'
+        client_connection.sendall(response.encode())
+        client_connection.close()
 
 # Close socket
 sserver.close()
